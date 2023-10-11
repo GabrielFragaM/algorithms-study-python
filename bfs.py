e8474766dfbed9f613839929cb0e83fbe4b9e6a0
Gabriel Fraga <68359082+GabrielFragaM@users.noreply.github.com>
2023-10-10 22:35:14 -0300

    first commit

---
+++ bfs.py
@@ -0,0 +1,61 @@
+# Depth First Search
+
+grafo = {
+  'A': ['B', 'C'],
+  'B': ['A', 'D', 'E'],
+  'C': ['A', 'F', 'G'],
+  'D': ['B'],
+  'E': ['B'],
+  'F': ['C'],
+  'G': ['C']
+}
+
+stack = [list(grafo.keys())[0]]  # Usamos uma pilha para DFS
+visitados = set()
+
+while stack:
+    node = stack.pop()  # Retire o nó do topo da pilha
+    print(f'\n\n----- Removendo o nó ({node}) do topo da pilha => {str(stack)} -------')
+    print(f'----- Verificando se o nó ({node}) está em visitados => {str(visitados)} -------')
+    if node not in visitados:
+        visitados.add(node)
+        print(f'----- Adicionando o nó ({node}) em visitado => {str(visitados)} -------')
+        for vizinho in grafo[node]:
+            print(f'----- Verificando o vizinho ({vizinho}) se está em visitados => {str(visitados)} -------')
+            if vizinho not in visitados:
+                print(f'----- Adicionando o vizinho ({vizinho}) na pilha => {str(stack)} -------')
+                stack.append(vizinho)
+
+        print(f'----- Pilha atualizada => {str(stack)} -------')
+
+
+
+#Breath First Search
+
+grafo = {
+  'A': ['B', 'C'],
+  'B': ['A', 'D', 'E'],
+  'C': ['A', 'F', 'G'],
+  'D': ['B'],
+  'E': ['B'],
+  'F': ['C'],
+  'G': ['C']
+}
+
+queue = [list(grafo.keys())[0]]
+visitados = set()
+
+while queue:
+    node = queue.pop(0)
+    print(f'\n\n----- Removendo o primeiro nó ({node}) da frente da fila => {str(queue)} -------')
+    print(f'----- Verificando se o nó ({node}) está em visitados => {str(visitados)} -------')
+    if node not in visitados:
+        visitados.add(node)
+        print(f'----- Adicionando o nó ({node}) em visitado => {str(visitados)} -------')
+        for vizinho in grafo[node]:
+            print(f'----- Verificando o vizinho ({vizinho}) se está em visitados => {str(visitados)} -------')
+            if vizinho not in visitados:
+                print(f'----- Adicionando o vizinho ({vizinho}) na fila => {str(queue)} -------')
+                queue.append(vizinho)
+
+        print(f'----- Fila atualizado => {str(queue)} -------')
